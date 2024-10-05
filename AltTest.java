9dda9cb5e0f76f43a699ba2c3d8396a142ffd755
koleastr <dasvipert@icloud.com>
2024-10-05 11:57:00 +0300

    Add files via upload

--- AltTest.java
+++ AltTest.java
@@ -1,16 +1,11 @@
 import java.util.Scanner;
 
 public class AltTest {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws Exception{
         Scanner sc = new Scanner(System.in);
         String input = sc.nextLine();
         String[] data;
         char act;
-
-
-        if (input.length() > 10) {
-            throw new Exception("Входная строка не должна превышать 10 символов");
-        }
 
 
         if (input.contains("+")) {
@@ -29,46 +24,52 @@ public class AltTest {
             throw new Exception("Некорректное действие");
         }
 
-        if (data.length < 2 || data[1].contains("\"")) {
-            throw new Exception("Делить и умножать можно только на число");
+        if (data.length != 2 || !data[0].startsWith("\"") || !data[0].endsWith("\"") || data[1].contains("\"")) {
+            throw new Exception("Некорректный ввод");
         }
 
-        int secondValue = Integer.parseInt(data[1]);
-        if (secondValue > 10) {
-            throw new Exception("Число не должно превышать 10");
+        String firstValue = data[0].substring(1, data[0].length() - 1);
+        if (firstValue.length() > 10) {
+            throw new Exception("Входная строка не должна превышать 10 символов");
+        }
+        int secondValue;
+
+        try {
+            secondValue = Integer.parseInt(data[1].trim());
+        } catch (NumberFormatException e) {
+            throw new Exception("Второе значение должно быть целым числом");
         }
 
-        for (int i = 0; i < data.length; i++) {
-            data[i] = data[i].replace("\"", "");
+        if (secondValue < 1 || secondValue > 10) {
+            throw new Exception("Число должно быть от 1 до 10 включительно");
         }
-
 
         String result = "";
-        if (act == '+') {
-            result = data[0] + data[1];
-        } else if (act == '*') {
-            result = data[0].repeat(secondValue);
-        } else if (act == '-') {
-            int index = data[0].indexOf(data[1]);
-            if (index == -1) {
-                result = data[0];
-            } else {
-                result = data[0].substring(0, index) + data[0].substring(index + data[1].length());
-            }
-        } else if (act == '/') {
-            int secondIndex = data[0].length() / secondValue;
-            result = data[0].substring(0, secondIndex);
+        switch (act) {
+            case '+':
+                result = firstValue + data[1].trim();
+                break;
+            case '*':
+                result = firstValue.repeat(secondValue);
+                break;
+            case '-':
+                result = firstValue.replace(data[1].trim(), "");
+                break;
+            case '/':
+                int length = firstValue.length() / secondValue;
+                result = firstValue.substring(0, length);
+                break;
+            default:
+                throw new Exception("Некорректное действие");
         }
-
 
         quot(result);
     }
 
     static void quot(String str) throws Exception {
         if (str.length() > 40) {
-            throw new Exception("Результирующая строка не должна превышать 40 символов");
-        } else {
-            System.out.println("\"" + str + "\"");
+            str = str.substring(0, 40) + "...";
         }
+        System.out.println("\"" + str + "\"");
     }
 }
